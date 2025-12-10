7bd5605b20e03cc009d521b33ce41fb32b2548fb
Kyle <vanoverloopkyle@gmail.com>
2025-12-10 06:18:48 +0100

    Update const.py

--- custom_components/easyplus_apex/const.py
+++ custom_components/easyplus_apex/const.py
@@ -3,10 +3,17 @@ from homeassistant.const import CONF_HOST, CONF_PORT, CONF_PASSWORD
 
 DOMAIN = "easyplus_apex"
 
-# Configuratie sleutels voor de Options Flow (Rolluiken)
+# Configuratie sleutels
 CONF_COVERS = "covers"
 CONF_COVER_NAME = "cover_name"
-CONF_ADDR_DIR = "address_direction" # Het relais voor Richting
-CONF_ADDR_POWER = "address_power"   # Het relais voor Stroom (Aan/Uit)
+CONF_ADDR_DIR = "address_direction"
+CONF_ADDR_POWER = "address_power"
 CONF_TRAVEL_TIME = "travel_time"
-CONF_INVERT_DIR = "invert_direction" # Optie om open/dicht om te wisselen
+CONF_INVERT_DIR = "invert_direction"
+
+# NIEUW: Voor XML Import
+CONF_XML_CONTENT = "xml_content"      # De ruwe XML tekst
+CONF_NAMING_MAP = "naming_map"        # Opslag: {adres: "Naam"}
+CONF_STRICT_MODE = "strict_mode"      # Als True: negeer alles wat niet in XML staat
+CONF_XML_SWITCHES = "xml_switches" # Lijst van adressen die ECHT switches zijn
+CONF_XML_DIMMERS = "xml_dimmers"   # Lijst van adressen die ECHT dimmers zijn
